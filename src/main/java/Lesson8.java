4aa0fd242fad2e3ba1e9d39e3b1819bbb44b8c6c
lyubovakr <46421675+lyubovakr@users.noreply.github.com>
2022-05-13 15:05:44 +0200

    Creat a new java class Lesson8.java

---
+++ src/main/java/Lesson8.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+public class Lesson8 {
+    public static void main(String[] args) {
+        int[] instArray = new  int[]{10,50,80,90,45};
+        instArray[0] = 10-25;
+        Integer[] integerArray = new Integer[6];
+      //  System.out.println(integerArray[0]);
+        System.out.println(Arrays.toString(instArray));
+        }
+
+
+    }
+
--- src/main/java/ListLinkedList.java
+++ src/main/java/ListLinkedList.java
@@ -6,6 +6,6 @@ public class ListLinkedList {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
         list = new LinkedList<>();
-        
+
     }
 }
