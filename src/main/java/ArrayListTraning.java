5558900242cbbc3e1537f0b5c647b9e74da5a432
lyubovakr <46421675+lyubovakr@users.noreply.github.com>
2022-05-13 12:31:43 +0200

    Second editting file ArrayListTraning

--- src/main/java/ArrayListTraning.java
+++ src/main/java/ArrayListTraning.java
@@ -3,11 +3,11 @@ import java.util.ArrayList;
 public class ArrayListTraning {
     public static void main(String[] args) {
         ArrayList<Integer> arrayList = new ArrayList<>();
-        for (int i = 0; i<100; i++) {
-            arrayList.add(i);
+        for (int x = 0; x<100; x++) {
+            arrayList.add(x);
         }
         System.out.println(arrayList);
-        System.out.println(arrayList.get(0));
-        System.out.println(arrayList.get(99));
+        System.out.println(arrayList.get(5));
+        System.out.println(arrayList.get(19));
     }
 }
