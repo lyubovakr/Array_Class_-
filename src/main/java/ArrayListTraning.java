c07840df9a1aa85a57ef1fb08e4a829706bcac59
lyubovakr <46421675+lyubovakr@users.noreply.github.com>
2022-05-13 13:09:41 +0200

    New function on file ArrayListTraning.java

--- src/main/java/ArrayListTraning.java
+++ src/main/java/ArrayListTraning.java
@@ -6,9 +6,12 @@ public class ArrayListTraning {
         for (int x = 0; x<100; x++) {
             arrayList.add(x);
         }
-        System.out.println(arrayList);
-        System.out.println(arrayList.get(5));
-        System.out.println(arrayList.get(19));
-        System.out.println(arrayList.size());
+        for (int x = 0; x< arrayList.size(); x++) {
+            System.out.println(arrayList.get(x));
+        }
+//        System.out.println(arrayList);
+//        System.out.println(arrayList.get(5));
+//        System.out.println(arrayList.get(19));
+//        System.out.println(arrayList.size());
     }
 }
