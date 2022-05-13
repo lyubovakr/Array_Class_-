8de38378ddc11491b49fe1e0fb7aa4fc2150189f
lyubovakr <46421675+lyubovakr@users.noreply.github.com>
2022-05-13 11:07:38 +0200

    First commit

---
+++ src/main/java/ArrayClass1.java
@@ -0,0 +1,11 @@
+public class ArrayClass1 {
+    public static void main(String[] args) {
+        int[] numbers = new int[5];
+        for (int i = 0; i<numbers.length; i++) {
+            numbers [i] = i*10;
+        }
+        for (int i = 0; i<numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
