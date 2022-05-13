dba8acd91c513821cbc5ffbd223a6aeb2eb9436a
lyubovakr <46421675+lyubovakr@users.noreply.github.com>
2022-05-13 13:37:34 +0200

    New function into file ArrayListTraning

--- src/main/java/ArrayListTraning.java
+++ src/main/java/ArrayListTraning.java
@@ -3,11 +3,16 @@ import java.util.ArrayList;
 public class ArrayListTraning {
     public static void main(String[] args) {
         ArrayList<Integer> arrayList = new ArrayList<>();
-        for (int x = 0; x<100; x++) {
+        for (int x = 0; x<10; x++) {
             arrayList.add(x);
         }
-        for (Integer i : arrayList)
-            System.out.println(i);
+            arrayList.add(11);
+            System.out.println(arrayList);
+            arrayList.remove(2);
+        System.out.println(arrayList);
+
+//        for (Integer i : arrayList)
+//            System.out.println(i);
 //        for (int x = 0; x< arrayList.size(); x++) {
 //            System.out.println(arrayList.get(x));
         }
