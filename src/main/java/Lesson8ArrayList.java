b68728beb709d2556e56b93786592e841974271a
lyubovakr <46421675+lyubovakr@users.noreply.github.com>
2022-05-13 17:51:31 +0200

    Add Link, Vector

--- src/main/java/Lesson8ArrayList.java
+++ src/main/java/Lesson8ArrayList.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Vector;
 
 public class Lesson8ArrayList {
     public static void main(String[] args) {
@@ -11,6 +13,26 @@ public class Lesson8ArrayList {
         myArray.add(0);
         myArray.add(0);
         myArray.set(2, 15);
+        myArray.set(5, 48);
+        myArray.set(1, 98);
         System.out.println(myArray);
+        for (int i = 0; i < myArray.size(); i++) {   //строки 17-18 идинтичне команде строк 21-22;
+            System.out.println(myArray.get(i));
+        }
+        System.out.println("''''''''''''''''");
+        for (Integer element : myArray) {    // == строкам 17-18;но этот цикл предпочтительней, не может быть ошибки
+            System.out.println(element);
+        }
+        LinkedList<Integer> link = new LinkedList<>();
+        link.add(10);
+        link.add(10);
+        link.set(1, 15);
+        System.out.println(link);
+        System.out.println(link.getLast());
+
+        Vector<Integer> vec = new Vector<>(0, 10);
+        vec.add(10);
+        System.out.println(vec);
+
     }
 }
