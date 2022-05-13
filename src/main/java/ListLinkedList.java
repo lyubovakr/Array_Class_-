226f26a265ce0ced5d7908558d863a3fb7bf6ebc
lyubovakr <46421675+lyubovakr@users.noreply.github.com>
2022-05-13 13:49:40 +0200

    Create anew file ListLinkedList.jav

---
+++ src/main/java/ListLinkedList.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ListLinkedList {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list = new LinkedList<>();
+        
+    }
+}
